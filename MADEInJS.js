a487d6d8900d93c1c57aa683ff641f8c62fa5260
TheCarlSaganExpress <TheCarlSaganExpress@gmail.com>
2017-10-16 16:57:46 -0400

    Fixes not having the proper Atari 2600 driver

--- MADEInJS.js
+++ MADEInJS.js
@@ -1,15 +1,11 @@
-
-  
-  var halo = {emuDriver: "a2600", emuJS: "mamea2600.js", gameFile: "atari_2600_halo_2600.bin", configFile: "a2600.cfg"  };
-	var curEmulator; 
-    //These should be make into JSON files instead
+    var curEmulator; 
     var temp = { "games":[{"emuDriver": "a2600", "emuJS": "mamea2600.js", "gameFile": "atari_2600_halo_2600.bin", "configFile": "a2600.cfg" },
                           {"emuDriver": "a2600", "emuJS": "mamea2600.js", "gameFile": "Space Invaders (1980) (Atari, Richard Maurer - Sears) (CX2632 - 49-75153).bin", "configFile": "a2600.cfg" },
                           {"emuDriver": "a2600", "emuJS": "mamea2600.js", "gameFile": "River Raid (1982) (Activision, Carol Shaw) (AX-020, AX-020-04).bin", "configFile": "a2600.cfg" },]};
-	function myFunction(gameNum){
-
-	if(curEmulator == null){
-        curEmulator = new Emulator(document.querySelector("#canvas"),
+    
+    function myFunction(gameNum){
+        if(curEmulator == null){
+            curEmulator = new Emulator(document.querySelector("#canvas"),
                             null,
                             new MAMELoader(MAMELoader.driver(temp.games[gameNum].emuDriver), //jsonContent.games[0].emuDriver
                                             MAMELoader.nativeResolution(256, 256),
@@ -21,7 +17,7 @@
                                                                 MAMELoader.fetchFile("Config File",
                                                                                         "examples/" + temp.games[gameNum].configFile)),
                                             MAMELoader.peripheral("cart", temp.games[gameNum].gameFile)))
-        curEmulator.setScale(2);
-        curEmulator.start({ waitAfterDownloading: true });
+            curEmulator.setScale(2);
+            curEmulator.start({ waitAfterDownloading: true });
+        }
     }
-  }
